fdff8e5b7a6a5e31087ffe70b97f95fd451e5f75
Peter Bellaby <bellaby1993@hotmail.co.uk>
2025-03-16 12:46:56 +0000

    created skill selection for pedd powers editor

--- personal_site/src/views/Admin/Components/SkillSelector.vue
+++ personal_site/src/views/Admin/Components/SkillSelector.vue
@@ -1,21 +1,30 @@
 <template>
-    <div class="mb-gap flex">
-        <label for="skills">Skills</label>
-        <p id="skills-display"></p>
-        <div class="flex">
-            <select>
+    <div class="mb-gap">
+        <div class="flex gap">
+            <p class="q-label bold">Skill Selection</p>
+            <select v-model="rank">
                 <option>Proficiency</option>
-                <option>Upgrade</option>
                 <option>Expertise</option>
             </select>
-            <select>
+            <select :value="choice" @change="selectChoice">
                 <option>Single</option>
                 <option>Selection of</option>
             </select>
-            <select>
-                <option v-for="skill in skills" :value="skill.skill">{{ skill.skill (skill.stat) }}</option>
+            <select v-model="chosenSkill">
+                <option v-for="skill in skills" :value="skill.skill">{{ skill.skill }} - {{ skill.category }} / {{
+                    skill.stat }}</option>
             </select>
-            <button class="btn">+</button>
+            <button type="button" class="btn" v-if="choice !== 'Single'" @click="addChoice">+</button>
+            <p id="choices-display" v-if="choices.length > 0">Choices: {{choices.map(s => s.skill).join(", ")}}</p>
+            <button type="button" class="btn" v-if="choices.length > 1" @click="choices.pop()">-</button>
+            <button type="button" class="btn" @click="addSkill"
+                v-if="choice == 'Single' || choices.length > 1">Add</button>
+        </div>
+        <div class="flex gap">
+            <p class="q-label">Skills:</p>
+            <p id="skills-display" class="w-fit">{{chosenSkills.map(s => Array.isArray(s) ? "[" + s.map(datum =>
+                datum.skill).join(", ") + "]" : s.skill).join(", ")}}</p>
+            <button type="button" class="btn" v-if="chosenSkills.length > 0" @click="chosenSkills.pop()">-</button>
         </div>
     </div>
 </template>
@@ -24,12 +33,67 @@
 import skillsData from '/src/assets/pedd/pedd-skills.json';
 import { ref } from 'vue';
 
-let skills = ref(
-    skillsData.basicSkills.concat(skillsData.knowledgeSkills.concat(skillsData.martialSkills))
-);
+let allSkills = [
+    ...skillsData.basicSkills.map(s => { return { ...s, category: "Core" } }),
+    ...skillsData.knowledgeSkills.map(s => { return { ...s, category: "Knowledge" } }),
+    ...skillsData.martialSkills.map(s => { return { ...s, category: "Martial" } })]
+    .sort((a, b) => a.skill.localeCompare(b.skill));
 
-console.log(skills.value);
+let skills = ref(
+    [{ skill: "Any", stat: "Any", category: "Any" },
+    { skill: "Any Core", stat: "Any", category: "Core" },
+    { skill: "Any Knowledge", stat: "Any", category: "Knowledge" },
+    { skill: "Any Martial", stat: "Any", category: "Martial" },
+    { skill: "Existing", stat: "Any", category: "Any" },
+    ...allSkills]
+);
+let rank = ref("Proficiency");
+let choice = ref("Single");
+let choices = ref([]);
+let chosenSkill = ref("Any");
+let chosenSkills = ref([]);
+
+function isString(v) {
+    return typeof v === 'string' || v instanceof String;
+}
+
+function getSkill() {
+    return skills.value.filter(s => s.skill == chosenSkill.value)[0];
+}
+
+function pushSkill(arr, skill) {
+    arr.push(skill);
+    arr = arr.sort((a, b) => {
+
+        if (isString(a.skill) && isString(b.skill)) return a.skill.localeCompare(b.skill);
+        else if (Array.isArray(a) && Array.isArray(b)) return a.length - b.length;
+        else if (Array.isArray(a)) return -1;
+        else return 1;
+    });
+}
+
+function addSkill() {
+    if (choice.value == "Single") {
+        pushSkill(chosenSkills.value, getSkill());
+    } else {
+        if (choices.value.length > 1) pushSkill(chosenSkills.value, choices.value);
+        choices.value = [];
+    }
+}
+
+function addChoice() {
+    pushSkill(choices.value, getSkill());
+}
+
+function selectChoice(e) {
+    choice.value = e.target.value;
+    choices.value = [];
+}
 
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.q-label {
+    width: 6rem;
+}
+</style>
