223a9a3d37e5f9cdd26354ba486a194ec1dc2096
Peter Bellaby <bellaby1993@hotmail.co.uk>
2025-10-25 23:39:32 +0100

    improved movement script, removed discovery

--- personal_site/src/assets/pedd/pedd_movement_1.js
+++ personal_site/src/assets/pedd/pedd_movement_1.js
@@ -1,31 +1,57 @@
-export default function(s) {
+export default function (s) {
     let reset = 300;
     let timer = 1;
 
     let dims = 400;
-    let size = 50;
-    let v = 0.0033;
+    let v = 0;
+    let vMax = 0.005;
 
-    let a = makeAgent(0.5, 0.1, "Mr A", "#F00");
-    let b = makeAgent(0.1, 0.8, "Mr B", "#FF0");
-    let c = makeAgent(0.9, 0.8, "Mr C", "#F0F");
+    let agents = [makeAgent(0.5, 0.1, "Mr A", "Mr B", "#0AC"), makeAgent(0.1, 0.8, "Mr B", "Mr C", "#AC0"), makeAgent(0.9, 0.8, "Mr C", "Mr A", "#C0A")];
 
     function drawAgent(agent) {
-        s.fill(agent.col);
-        s.circle(agent.x * dims, agent.y * dims, size);
-        s.textAlign(s.CENTER, s.CENTER);
-        s.fill("black")
-        s.text(agent.name, agent.x * dims, agent.y * dims + 2);
     }
 
-    function makeAgent(x, y, name, col) {
+    function makeAgent(x, y, name, target, col) {
         return {
             x: x,
             startX: x,
             y: y,
             startY: y,
+            r: 0.075,
             name: name,
-            col: col
+            target: target,
+            col: col,
+            move() {
+                let b = agents.filter(agent => agent.name == this.target)[0];
+
+                //get unit vector in direction of target and scale by v
+                let dir = s.createVector(b.x, b.y).sub(this.x, this.y);
+
+                if (dir.mag() > this.r * 2) {
+                    let change = dir.normalize().mult(v);
+                    this.x += change.x;
+                    this.y += change.y;
+                }
+            },
+            offset(other) {
+                //offset to be size
+                let targetPos = s.createVector(other.x, other.y);
+                let dir = s.createVector(this.x, this.y).sub(targetPos);
+                let distanceOverlapping = this.r - dir.mag()
+                let offset = dir.normalize().mult(distanceOverlapping);
+                let newPos = s.createVector(this.x, this.y).add(offset);
+                this.x = newPos.x;
+                this.y = newPos.y;
+            },
+            draw() {
+                s.fill("white")
+                s.circle(this.x * dims, this.y * dims, this.r * 2 * dims)
+                s.fill(this.col);
+                s.circle(this.x * dims, this.y * dims, this.r * 2 * dims - 4);
+                s.textAlign(s.CENTER, s.CENTER);
+                s.fill("black")
+                s.text(this.name, this.x * dims, this.y * dims);
+            }
         }
     }
 
@@ -37,53 +63,29 @@ export default function(s) {
         //s.clear();
 
         s.stroke('#fff');
-        s.rect(0,0,s.width-2, s.height-2);
+        s.rect(0, 0, s.width - 2, s.height - 2);
         s.stroke('#0000');
 
         //move to target
-        moveAgentToAgent(a, b);
-        moveAgentToAgent(b, c);
-        moveAgentToAgent(c, a);
-
-        drawAgent(a);
-        drawAgent(b);
-        drawAgent(c);
+        for (let a of agents) a.move()
+        for (let a of agents) a.draw()
 
         if (timer++ % reset == 0) {
             timer = 1;
-            resetAgent(a);
-            resetAgent(b);
-            resetAgent(c);
+            v = 0;
+            resetAgents()
             s.clear();
         }
-    }
-
-    function resetAgent(a) {
-        a.x = a.startX;
-        a.y = a.startY;
-    }
-
-    function moveAgentToAgent(a, b) {
-        let target = s.createVector(b.x, b.y);
-        let current = s.createVector(a.x, a.y);
-
-        //get unit vector in direction of target and scale by v
-        let dir = target.sub(current);
-        let dist = Math.sqrt(dir.x * dir.x + dir.y * dir.y);
-        
-        if(dist > size/dims) {
-            let change = dir.normalize().mult(v);
-            a.x += change.x;
-            a.y += change.y;
-        }
         else {
-            target = s.createVector(b.x, b.y);
-            current = s.createVector(a.x, a.y);
-            let dirBtoA = current.sub(target).normalize();
-            let offset = dirBtoA.mult(size / dims);
-            let newPos = target.add(offset);
-            a.x = newPos.x;
-            a.y = newPos.y;
+            v += 0.00005;
+            if(v > vMax) v = vMax;
+        }
+    }
+
+    function resetAgents() {
+        for (let a of agents) {
+            a.x = a.startX;
+            a.y = a.startY;
         }
     }
 }
